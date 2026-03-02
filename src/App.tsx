409b3cb3a333dec580c5cd3f7327d163c9315020
Caique Schinaider <168691564+CaiqueSchinaider@users.noreply.github.com>
2026-03-02 16:10:13 -0300

    fix: logo

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
 import { Toaster } from 'react-hot-toast'
 import './App.css'
 import AppRouter from './Router/router'
-import './Styles/hideScrollbar.css';
+import './styles/hideScrollbar.css';
 
 
 function App() {
