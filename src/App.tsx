9a10efb2fff54372590c65d0e0dfa1e4672b38c4
Caique Schinaider <168691564+CaiqueSchinaider@users.noreply.github.com>
2026-03-03 15:30:59 -0300

    Fix template

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
 import { Toaster } from 'react-hot-toast'
 import './App.css'
 import AppRouter from './Router/router'
-import './styles/hidescrollbar.css';
+import './Styles/hidescrollbar.css';
 
 
 function App() {
