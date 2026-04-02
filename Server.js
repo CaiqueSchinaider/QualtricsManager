5473e6d803e4df3cc5e0d70aaa118b75cb70418f
Caique Schinaider <168691564+CaiqueSchinaider@users.noreply.github.com>
2026-04-02 11:01:39 -0300

    Deploy cors

--- Server.js
+++ Server.js
@@ -26,6 +26,11 @@ const db = getFirestore(app);
 
 const appServer = express()
 
+import cors from "cors";
+
+app.use(cors({
+  origin: "https://qualtrics-manager.vercel.app"
+}));
 appServer.use(express.json())
 
 appServer.get('/api/scripts', async (req, res) => {
